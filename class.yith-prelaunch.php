7ac266f8f2c154a3f11ff118c828f40ad5e0c78b
yithemes <yithemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-23 07:32:30 +0000

    Version 1.0.5
    git-svn-id: https://plugins.svn.wordpress.org/yith-pre-launch/tags/1.0.5@995244 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ class.yith-prelaunch.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Main class
+ *
+ * @author Your Inspiration Themes
+ * @package YITH Pre-Launch
+ * @version 1.0.2
+ */
+
+if ( !defined( 'YITH_PRELAUNCH' ) ) { exit; } // Exit if accessed directly
+
+if( !class_exists( 'YITH_Prelaunch' ) ) {
+    /**
+     * YITH Pre-Launch
+     *
+     * @since 1.0.0
+     */
+    class YITH_Prelaunch {
+        /**
+         * Plugin version
+         *
+         * @var string
+         * @since 1.0.0
+         */
+        public $version = '1.0.5';
+
+        /**
+         * Plugin object
+         *
+         * @var string
+         * @since 1.0.0
+         */
+        public $obj = null;
+
+        /**
+         * Constructor
+         *
+         * @return mixed|YITH_Prelaunch_Admin|YITH_Prelaunch_Frontend
+         * @since 1.0.0
+         */
+        public function __construct() {
+            if( is_admin() ) {
+                $this->obj = new YITH_Prelaunch_Admin( $this->version );
+            } else {
+                $this->obj = new YITH_Prelaunch_Frontend( $this->version );
+            }
+
+            return $this->obj;
+        }
+    }
+}
